703e1e08081c2c2b881c9f475bf119cf404b9f83
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-12 21:28:12 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,21 @@
+package javaLabCard1;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		byte b=16;
+		
+		int i=b; 
+		byte x=1;
+		
+		byte r=(byte)i;
+		byte t=23;
+		byte h=03;
+		byte u=05;
+		
+		byte sum=u*t;
+		
+		
+		
+	}
+
+}
