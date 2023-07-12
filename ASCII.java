1cfa7eae7cccb2bf342d588dd49b543803524180
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-12 21:49:33 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,15 @@
+package javaLabCard1;
+
+public class ASCII {
+   public static void main(String[] args) {
+	char ch = 'R';
+	int ASCII = ch;
+	//I used here to convert char to int
+	
+	int castASCII =(int) ch;
+	
+	System.out.println("The ASCII Value of "+ ch +" is : "+ ASCII);
+    System.out.println("The ASCII Value of "+ ch +" is : "+ castASCII);	
+    }
+
+}
