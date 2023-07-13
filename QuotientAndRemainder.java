2f379a782015a4a532fc6adaad591adeb25fc8eb
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-13 23:08:18 +0530

    Add files via upload

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,18 @@
+package javaLabCard1;
+
+public class QuotientAndRemainder {
+    private static int dividend;
+	private static int divisor;
+
+	public static void main(String[] args) {
+		int dividend = 16, divisor = 23;
+		
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+		
+		System.out.println("Quotient =" + quotient);
+		System.out.println("Remainder ="+ remainder);
+		
+	}
+
+}
