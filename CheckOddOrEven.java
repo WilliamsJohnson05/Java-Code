1b454b271c5d6ffe7ff1be164633ed5a341c6395
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-13 23:08:44 +0530

    Add files via upload

---
+++ CheckOddOrEven.java
@@ -0,0 +1,19 @@
+package javaLabCard1;
+
+import java.util.Scanner;
+
+public class CheckOddOrEven {
+	public static void main(String[] args) {
+		int val;
+		System.out.println("Emter an Integer Number");
+		
+		Scanner input = new Scanner(System.in);
+		val = input.nextInt();
+		
+		if(val % 2==0)
+		  System.out.println("What Number you have Entered is Even");
+		else
+		  System.out.println("What Number you have Entered is Odd");
+		}
+
+}
