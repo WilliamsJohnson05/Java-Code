04f578e262284254872a75691fecee87f215ec38
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-15 20:51:29 +0530

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+package javaLabCard1;
+
+public class SwapTwoNumbers {
+public static void main(String[] args) {
+	float first = 2.3f, second = 1.6f;
+	
+	System.out.println("Before Swap.... :");
+	System.out.println("First Number= "+ first);
+	System.out.println("Second Number="+ second);
+	
+	float temp = first;
+	first = second;
+	
+	
+	System.out.println("After swap.... :");
+	System.out.println("First Number= "+ first);
+	System.out.println("Second Number= "+ second);
+	
+	
+}}
