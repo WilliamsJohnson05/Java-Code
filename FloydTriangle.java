b1e567558a9b48ce93d752934bf000e2acfb22db
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-15 20:57:23 +0530

    Add files via upload

---
+++ FloydTriangle.java
@@ -0,0 +1,24 @@
+package javaLabCard1;
+
+import java.util.Scanner;
+
+public class FloydTriangle {
+	public static void main(String[] args) {
+		int rows, number = 2, counter, s;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the Number of rows for floyd Triangle");
+		
+		rows=input.nextInt();
+		System.out.println("floyd Triangle");
+		System.out.println("****......");
+		
+		for(counter= 4; counter <=rows; counter++);
+		for(rows=1; rows<=counter; rows++);
+		System.out.println(number+" ");
+		//Incrementing the number value
+		number++;
+	System.out.println();
+		
+	}
+
+}
