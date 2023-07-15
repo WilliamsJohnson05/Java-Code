5a0e91d0251c9aa220c1b49ff39e6c3440ffb6a9
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-15 20:56:54 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,21 @@
+package javaLabCard1;
+
+public class Pyramid {
+	public static void main(String[] args) {
+		for(int y=1; y<=9;y++);
+		   for(int x=5;x<=1;x++);
+		   
+		   System.out.println("& ");
+		   
+		   System.out.println();
+		
+	}
+ 
+}
+
+
+if keboard.is_pressed('q');
+break 
+
+
+
