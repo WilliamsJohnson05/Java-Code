7217e9f01d9af456fdff7b75295cf40b56077494
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-16 22:27:32 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,23 @@
+package javaLabCard1;
+import java.util.Scanner;
+
+public class AreaOfCircle {
+	
+	public static void main(String args[])
+	 {
+	 Scanner scanner = new Scanner(System.in);
+	 System.out.print("Enter the radius: ");
+	 	 double radius = scanner.nextDouble();
+	 	 
+	 //Area = PI*radius*radius
+	 	 
+	double area = Math.PI * (radius * radius);
+	 System.out.println("The area of circle is: " + area);
+	 
+	 //Circumference = 2*PI*radius
+	 
+	double Circumference= Math.PI * 2*radius;
+	 System.out.println( "The Circumference of the Circle is: "+ Circumference);
+}
+}
+
---
+++ AreaofaSquare.java
@@ -0,0 +1,21 @@
+package javaLabCard1;
+import java.util.Scanner;
+
+public class AreaofaSquare {
+	
+	class FindAreaOfSquare
+	{
+	public static void main (String[] args)
+	 {
+	 System.out.println("Enter Side of Square:");
+	 //Capture the user's input
+	 Scanner scanner = new Scanner(System.in);
+	 //Storing the captured value in a variable
+	double side = scanner.nextDouble();
+	 //Area of Square = side*side
+	double area = side*side;
+	 System.out.println("Area of Square is: "+area); 
+}
+	}
+}
+	
