a17d18b985de6bec6e52e94fc8c544c099a3f602
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-16 22:24:13 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,25 @@
+package javaLabCard1;
+public class SwitchCase {
+	
+
+public static void main(String args[]){
+ int Tech = 3;
+
+ switch(Tech){
+case 1:
+ {
+ System.out.println("Smart Phone.....");
+ break;
+ }
+ case 2:{
+ System.out.println("Laptops.....");
+ break;
+ }
+ default:{
+ System.out.println("Airpods....");
+    }
+ }
+}
+}
+ 
+
