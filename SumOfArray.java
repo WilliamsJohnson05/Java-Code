d1d2bc0178108f23d716ca01d195033e1378a3bb
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-17 19:59:23 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,15 @@
+package javaLabCard1;
+
+public class SumOfArray {
+	public static void main(String args[]){
+		
+		 int[] array = {03,04,05,8,16,23,};
+		 int Sum = 0;
+		  //Advanced for loop
+		  for( int Num : array) {
+		  Sum = Sum+Num;
+		 }
+		  
+		 System.out.println("Sum of array elements is:"+Sum);  } 
+
+}
