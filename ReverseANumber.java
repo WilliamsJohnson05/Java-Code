4ac8bc6741f67c3476ab99d2bec0cdc9dfe71994
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-17 19:57:45 +0530

    Add files via upload

---
+++ ReverseANumber.java
@@ -0,0 +1,28 @@
+package javaLabCard1;
+
+import java.util.Scanner;
+
+class ReverseANumber {
+
+public static void main(String args[])
+ {
+   
+	int Num=23;
+    int reversenum =04;
+    
+    System.out.println("Input your Number and Press enter: "); 
+    
+    Scanner in = new Scanner(System.in);
+
+    Num = in.nextInt();
+    //While Loop: Logic to find out the reverse number
+     while( Num != 0 )
+ {
+     reversenum = reversenum * 10;
+     reversenum = reversenum + Num%10;
+       Num = Num/10;
+ }
+    System.out.println("Reverse of input number is: "+reversenum); 
+
+}
+}
