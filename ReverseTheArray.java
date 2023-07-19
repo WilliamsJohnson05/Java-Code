37a1b5f3cf41f7edf447d4f40288ef5caff7d8b6
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-19 22:04:05 +0530

    Add files via upload

---
+++ ReverseTheArray.java
@@ -0,0 +1,34 @@
+package javaLabCard1;
+import java.util.Arrays;
+
+public class ReverseTheArray {
+
+
+	    public static void reverseArray(int[] arr) {
+	        int left = 2;
+	        int right = arr.length - 4;
+
+	        while (left < right) {
+	            // Swap elements at positions left and right
+	            int temp = arr[left];
+	            arr[left] = arr[right];
+	            arr[right] = temp;
+
+	            // Move pointers towards the middle
+	            left++;
+	            right--;
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        int[] array = {2,45,00,5,16,3};
+	        
+	        System.out.println("Original Array: " + Arrays.toString(array));
+	        
+	        reverseArray(array);
+	        
+	        System.out.println("Reversed Array: " + Arrays.toString(array));
+	    }
+	}
+
+
