ff20caf4ce70cbda280babb48ca55c93f0dde5bb
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-26 21:12:58 +0530

    Add files via upload

---
+++ TwoDRectangularArray.java
@@ -0,0 +1,31 @@
+package CloseLabBook;
+
+public class TwoDRectangularArray {
+
+	    public static void main(String[] args) {
+	        int Rows = 4;
+	        int Columns = 4;
+	        int num = 1;
+	        
+	        int[][] matrix = new int[Rows][Columns];
+	        
+	        
+	        // Fill the matrix with numbers
+	        for (int x= 0; x < Rows; x++) {
+	            for (int r = 0; r < Columns; r++) {
+	                matrix[x][r] = num;
+	                num++;
+	            }
+	        }
+	        
+	        // Display the matrix
+	        for (int y = 0; y < Rows; y++) {
+	            for (int z = 0; z < Columns; z++) {
+	                System.out.printf("%2d ", matrix[y][z]);
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
+
+
