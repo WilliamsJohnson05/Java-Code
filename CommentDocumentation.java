dc7933cc3071b02ccca2a35f1fa4d4b5e4e6e2f8
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-29 23:06:07 +0530

    Add files via upload

---
+++ CommentDocumentation.java
@@ -0,0 +1,47 @@
+package CloseLabBook;
+
+public class CommentDocumentation {
+	/**
+	 * This is a sample Java program with comment documentation.
+	 * It demonstrates different ways to define the main method and its execution.
+	 */
+
+	    /**
+	     * This is the main method with public and static specifiers, and it takes an array of strings as parameters.
+	     * It prints a simple message to the console.
+	     */
+	    public static void main(String[] args) {
+	        System.out.println("Executing main method with public and static specifiers: ");
+	    }
+
+	    /**
+	     * This is the main method without the public specifier. It cannot be executed directly from the command line.
+	     * It prints a message to the console.
+	     */
+	     
+	   public static void mainWithoutPublicSpecifier(String[] args) {
+	        System.out.println("Executing main method without the public specifier: ");
+	    }
+
+	    /**
+	     * This is the main method without the static modifier. It cannot be executed directly from the command line.
+	     * It prints a message to the console.
+	     */
+	     
+	    public void mainWithoutStaticModifier(String[] args) {
+	        System.out.println("Executing main method without the static modifier: ");
+	    }
+
+	    /**
+	     * This is the main method without function parameters. It cannot be executed directly from the command line.
+	     * It prints a message to the console.
+	     */
+	    public static void mainWithoutFunctionParameter() {
+	        System.out.println("Executing main method without function parameters: ");
+	    }
+	}
+
+
+
+
+
