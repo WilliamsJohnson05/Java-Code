9f1f50317eae67102990cad697afc922e58ebe5d
Williams Johnson <137495887+WilliamsJohnson05@users.noreply.github.com>
2023-07-29 23:02:06 +0530

    Add files via upload

---
+++ CommandlineArguments.java
@@ -0,0 +1,18 @@
+package CloseLabBook;
+
+public class CommandlineArguments {
+	public class CommandLineArgumentsExample {
+	    public static void main(String[] args) {
+	        if (args.length == 0) {
+	            System.out.println("No command-line arguments provided.");
+	        } else {
+	            System.out.println("Command-line arguments:");
+	            for (int i = 0; i < args.length; i++) {
+	                System.out.println("Argument " + (i + 1) + ": " + args[i]);
+	            }
+	        }
+	    }
+	}
+}
+	    
+
